916c678801b07cbfcc330c7bf28b2cb5ff0e6d36
Imtiyaaz Thompson <imtiyaaz.thompson@impact.com>
2025-06-12 12:15:15 +0200

    foundations of delta time and fps limiting. Moving to separate branch to do a quick implementaion of a fps counter

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import time
 import pygame
 from pygame.locals import *
 
@@ -9,7 +10,20 @@ _height = 600
 screen = pygame.display.set_mode((_width, _height))
 
 running = True
+prev_time = time.time()
+dt = 0
+elapsed = 0
+fps = 30
+clock = pygame.time.Clock()
+
+# Game loop from start to finish is considered 1 frame
 while running:
+    clock.tick(fps) # limit fps, ensure framerate does not exceed fps value    
+
+    curr_time = time.time()
+    dt = curr_time - prev_time
+    elapsed += dt
+
     screen.fill((255,255,255))
 
     for event in pygame.event.get():
